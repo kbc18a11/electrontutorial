4cfb87d7b8dc5fde8eed6e57d8ed707608cfc646
kbc18a11 <wada121400@gmail.com>
2020-11-27 10:25:58 +0900

    プロセス館通信何度なく分かった

--- src/a.js
+++ src/a.js
@@ -17,3 +17,16 @@ function displayNumber() {
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+const reloadButton = document.getElementsByClassName('reload')[0];
+reloadButton.addEventListener('click', () => {
+    doReload();
+});
+
+const tcpButton = document.getElementsByClassName('tcp')[0];
+tcpButton.addEventListener('click', () => {
+    // レンダラプロセス（送信側）
+    const electron = require('electron');
+    const ipc = electron.ipcRenderer;
+    console.log(ipc.sendSync('synchronous-message', 'ping'));// 'message'というイベントを実行
+});
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipc = electron.ipcMain;
 
 let mainWindow = null;
 app.on('ready', () => {
     // mainWindowを作成（windowの大きさや、Kioskモードにするかどうかなどもここで定義できる）
-    mainWindow = new BrowserWindow({ width: 800, height: 800 });
+    mainWindow = new BrowserWindow({
+        width: 800, height: 800, webPreferences: {
+            nodeIntegration: true
+        }
+    });
     // Electronに表示するhtmlを絶対パスで指定（相対パスだと動かない）
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
@@ -15,4 +20,10 @@ app.on('ready', () => {
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
+});
+
+// ipc.on(channel, listener) の形をとる。channel 名はなんでも良い。
+ipc.on('synchronous-message', (event, arg) => {
+    console.log("arg: ", arg);  // arg には 'hoge' が入る (後述)
+    event.returnValue = "Okay"; //レンダラープロセスに返す
 });
